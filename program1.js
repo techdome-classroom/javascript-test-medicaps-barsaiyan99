837de1b8f4ecaf2a267c619e673515be6e0b0606
= <naman.barsaiyan2001@gmail.com>
2024-04-25 13:43:14 +0530

    both program solved

--- program1.js
+++ program1.js
@@ -1,7 +1,22 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
-}
+    let maxLength = 0;
+    let start = 0;
+    let charIndexMap = {};
 
+    for (let end = 0; end < s.length; end++) {
+        const char = s[end];
+        if (charIndexMap[char] !== undefined && charIndexMap[char] >= start) {
+            start = charIndexMap[char] + 1;
+        }
+        charIndexMap[char] = end;
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+
+    return maxLength;
+
+}
 module.exports = { longestSubstring };
+
 
 
